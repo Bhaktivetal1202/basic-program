4e481976202c4b85462a05ec1384f1f43f22dbf3
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-10-18 18:51:52 +0530

    Add files via upload

---
+++ basic1.cpp
@@ -0,0 +1,13 @@
+/* c program to accept two numbers from user and perform all arithmetic operations on them */
+#include<stdio.h>
+void main()
+{
+	int x,y;
+	printf("enter two numbers=");
+	scanf("%d%d", &x, &y);
+	printf("\n addition=%d",x+y);
+	printf("\n substraction=%d",x-y);
+	printf("\n multiplication=%d",x*y);
+	printf("\n division=%d",x/y);
+	printf("\n modular division=%d",x%y);
+}
