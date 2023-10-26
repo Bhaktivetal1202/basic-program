94615e018d2e85a3d368e34dcb92c2cf557de408
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-10-26 18:09:43 +0530

    Add files via upload

---
+++ basic9.cpp
@@ -0,0 +1,13 @@
+/* c program to accept three dimensions length (l), breadth (b) and height (h) of a cuboid and print surface area and volume */
+#include<stdio.h>
+void main()
+{
+	float l,b,h,sarea,volume;
+	printf("enter length,breadth,height of a cuboid=");
+	scanf("%f%f%f",&l,&b,&h);
+	sarea=2*((l*b)+(l*h)+(b*h));
+        volume=l*b*h;
+	printf("\n surface area of a cuboid=%.2f",sarea);
+        printf("\n volume of a cuboid=%.2f", volume);
+}
+
